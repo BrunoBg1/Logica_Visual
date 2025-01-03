30c6049ef0e2c5d0321bc3a8da8ce9ad0a5b2f90
unknown <Oliveira30rs@gmail.com>
2025-01-02 23:29:25 -0300

    Adicionando parâmetro a função

--- logica-js-projeto_visual/app.js
+++ logica-js-projeto_visual/app.js
@@ -1,5 +1,11 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+function exibirTextoTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto
+}
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+exibirTextoTela('h1', 'Jogo do número secreto');
+exibirTextoTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+        console.log('O botão foi clicado!');
+}

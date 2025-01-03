272c61111a0f7318ad7e8601672813d42b834da3
unknown <Oliveira30rs@gmail.com>
2025-01-03 00:20:51 -0300

    Gerando randomicamente o número secreto e colocando a possibilidade do chute

--- logica-js-projeto_visual/app.js
+++ logica-js-projeto_visual/app.js
@@ -1,3 +1,4 @@
+let numeroSecreto = gerarNumeroAleatorio();
 function exibirTextoTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto
@@ -7,5 +8,20 @@ exibirTextoTela('h1', 'Jogo do número secreto');
 exibirTextoTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-        console.log('O botão foi clicado!');
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        exibirTextoTela('h1', 'Você acertou!');
+        exibirTextoTela('p', 'Parabéns você descobriu o número secreto!');
+    } else{
+        if (chute > numeroSecreto) {
+            exibirTextoTela('p', 'O número é menor!');
+        }else{
+            exibirTextoTela('p', 'O número é maior!');
+        }
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
